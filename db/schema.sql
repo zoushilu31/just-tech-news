c17f40552b3cb52f77a6e163ff73da4604a8efce
shilu <zoushilu@gmail.com>
2020-11-07 13:21:39 -0800

    finished

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,3 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
 
 CREATE DATABASE just_tech_news_db;
-
-USE employeeDB;
-
-CREATE TABLE department (
-  id INI auto_increment Primary Key Not Null,
-  name VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE role (
-    id INT auto_increment PRIMARY KEY NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(9,2) NOT NULL,
-    department_id INT NOT NULL
-);
-
-
-CREATE TABLE employee (
-    id INT auto_increment PRIMARY KEY NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
-);
--- models/User.js
+++ models/User.js
@@ -8,8 +8,50 @@ class User extends Model {}
 User.init(
   {
     //table column definitions go here
+    id: {
+      type: DataTypes.INTEGRE,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    //define a username column
+    username: {
+      type: DataTypes.String,
+      allowNull: false
+    },
+    //define an email column
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail:true
+      }
+    },
+    //define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+
   // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
